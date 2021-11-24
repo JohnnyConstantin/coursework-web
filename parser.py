dd7a61fbd4513a42fc413b453a040b2428f53abe
Krasav4ik <zkonst12@gmail.com>
2021-11-25 01:41:37 +0300

    Added errors templates, only laptop search left

--- parser.py
+++ parser.py
@@ -1,11 +1,12 @@
-from flask import request, Flask, render_template, request
+from flask import request, Flask, render_template, request, jsonify, make_response
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+
 def parse_mobicom(item):
-    #For data
+    # For data
     d = []
 
     # get pages
@@ -18,32 +19,42 @@ def parse_mobicom(item):
         r = session.get(url, params=par)
         # получаем объект  BeautifulSoup и записываем в переменную soup
         soup = BeautifulSoup(r.text, 'html.parser')
-        # с помощью циклам перебiраем товары на странице и получаем из них нужные параметры
+        # с помощью циклам перебираем товары на странице и получаем из них нужные параметры
         for i in range(12):
             # получаем название товара
             product = soup.find_all(class_='prod-en-lista-name')[i].get_text()
-            if(item in product):
+            if (item in product):
                 # получаем цену товара
                 price = soup.find_all(class_='prod-en-lista-price')[i].get_text()
                 # удаляем пробел из цены
                 price = price.replace(" ", "")
                 price = price.replace("\n", "")
-                # получаем ссылку на товар
                 # добавляем данные о товаре в список
                 d.append([product, price])
-    for a in d:
-        print(a, end='\n')
+    return d
+
 
 @app.route('/')
 def start():
     return render_template('index.html')
 
+
 @app.route('/get_laptops', methods=['POST'])
 def get_laptops():
-    #here will be parsing
+    # here will be parsing
+
     checked = request.form.get('laptop')
     item = request.form.get('item_to_search')
-    print(checked)
-    parse_mobicom(item)
+
+    if item.replace(" ", "") != "":
+        d = parse_mobicom(item)
+        if len(d) == 0:
+            print()
+            return render_template('not_found.html')
+        else:
+            print()
+            #return  Вывести предметы
+    else:
+        return render_template('no_args.html')
 
     return render_template('index.html')
--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,6 @@
 window.onload = function () {
 
-    document.querySelector('#type_phones').onclick = function() {
-       if ( this.checked ) {
-           console.log("phone checked");
-            document.querySelector('.type .fa-phone').onclick = function() {
-                this.setAttribute("style", "font-size: 35px")
-            }
-        } else {
-           document.querySelector('.type .fa-phone').onclick = function() {
-                this.setAttribute("style", "border-radius: 40px; font-size: 30px; background-color: chocolate;")
-           }
-        }
-    }
-
-    document.querySelector('#type_laptops').onclick = function() {
-        // access properties using this keyword
-        if ( this.checked ) {
-            document.querySelector('.type .fa-laptop').onclick = function() {
-                this.setAttribute("style", "font-size: 35px")
-            }
-        } else {
-        document.querySelector('.type .fa-laptop').onclick = function() {
-                this.setAttribute("style", "border-radius: 40px; font-size: 30px; background-color: chocolate;")
-             console.log("laptop checked")
-            }
-        }
+    document.querySelector("#submit_button").onclick = function () {
+        document.querySelector("#searching").innerHTML = "Идет поиск...";
     }
 }
