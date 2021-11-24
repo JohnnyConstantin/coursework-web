d89d3685a9f3257bc3d17d151cbf86deafb78ae7
Krasav4ik <zkonst12@gmail.com>
2021-11-24 15:11:52 +0300

    Done checking

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,29 @@
-document.getElementById('laptop').onclick = function() {
-    // access properties using this keyword
-    if ( this.checked ) {
-        // Returns true if checked
-        alert( this.value );
-    } else {
-        // Returns false if not checked
+window.onload = function () {
+    
+    document.querySelector('#type_phones').onclick = function() {
+       if ( this.checked ) {
+           console.log("phone checked");
+            document.querySelector('.type .fa-phone').onclick = function() {
+                this.setAttribute("style", "font-size: 35px")
+            }
+        } else {
+           document.querySelector('.type .fa-phone').onclick = function() {
+                this.setAttribute("style", "border-radius: 40px; font-size: 30px; background-color: chocolate;")
+           }
+        }
     }
-};
+
+    document.querySelector('#type_laptops').onclick = function() {
+        // access properties using this keyword
+        if ( this.checked ) {
+            document.querySelector('.type .fa-laptop').onclick = function() {
+                this.setAttribute("style", "font-size: 35px")
+            }
+        } else {
+        document.querySelector('.type .fa-laptop').onclick = function() {
+                this.setAttribute("style", "border-radius: 40px; font-size: 30px; background-color: chocolate;")
+             console.log("laptop checked")
+            }
+        }
+    }
+}
